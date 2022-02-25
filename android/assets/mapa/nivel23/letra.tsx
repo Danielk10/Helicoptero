acbdcbc572923c8325ec49e86bec74c82ac2737f
Danielk10 <danielpdiamon@gmail.com>
2022-02-25 11:05:00 -0430

    correccion de mapas

---
+++ android/assets/mapa/nivel23/letra.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="letra" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="letra.png" width="96" height="96"/>
+</tileset>
