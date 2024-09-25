581ea19b15f9ddbb46297e50b11470747b8a8200
Danielk10 <danielpdiamon@gmail.com>
2024-09-25 14:17:43 -0400

    update AndroidLauncher.java

--- android/src/main/java/com/diamon/helicoptero/android/AndroidLauncher.java
+++ android/src/main/java/com/diamon/helicoptero/android/AndroidLauncher.java
@@ -24,15 +24,91 @@ import com.diamon.helicoptero.Helicoptero;
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
+  
+    private WakeLock wakeLock;
+  
+  	private PantallaCompleta pantallaCompleta;
+  
+  	private MostrarPublicidad publicidad;
+  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         AppCenter.start(getApplication(), "2dea6be0-8f41-49ac-b4ce-468cced27237",
                   Analytics.class, Crashes.class);
+                  
+        pantallaCompleta = new PantallaCompleta(this);
+        
+        pantallaCompleta.pantallaCompleta();
+        
+        pantallaCompleta.ocultarBotonesVirtuales();
+        
+        publicidad = new MostrarPublicidad(this);
+        
+        publicidad.IniciarPublicidad(this);
         
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        
+        RelativeLayout mainLayout = new RelativeLayout(this);
+        
+        FrameLayout frame = new FrameLayout(this);
+        
         configuration.useImmersiveMode = true; // Recommended, but not required.
-        initialize(new Helicoptero(), configuration);
+        
+        RelativeLayout.LayoutParams mrecParameters = new RelativeLayout.LayoutParams(
+        				RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        		mrecParameters.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        		mrecParameters.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        
+        		mainLayout.addView(baner, mrecParameters);
+        
+        		frame.addView(initializeForView(new Helicoptero(publicidad), configuration), new FrameLayout.LayoutParams(
+        				FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+        
+        		frame.addView(mainLayout);
+        
+        		setContentView(frame);
+        
+        		PowerManager powerManejador = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        
+        		wakeLock = powerManejador.newWakeLock(PowerManager.FULL_WAKE_LOCK, "GLGame");
     }
+    
+    @Override
+    	protected void onPause() {
+    
+    		super.onPause();
+    
+    		wakeLock.release();
+    
+    	}
+    
+    	@Override
+    	protected void onResume() {
+    
+    		super.onResume();
+    
+    		wakeLock.acquire();
+    
+    	}
+    
+    	@Override
+    	public void onWindowFocusChanged(boolean hasFocus) {
+    		super.onWindowFocusChanged(hasFocus);
+    
+    		if (hasFocus) {
+    
+    			pantallaCompleta.ocultarBotonesVirtuales();
+    		}
+    
+    	}
+    
+    	@Override
+    	public boolean onKeyUp(int keyCode, KeyEvent event) {
+    
+    		pantallaCompleta.ocultarBotonesVirtuales();
+    
+    		return super.onKeyUp(keyCode, event);
+    	}
 }
